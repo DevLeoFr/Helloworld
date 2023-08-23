d003fd8f7874f36b446d3cd86eca073adfa9ac34
DevLeoFr <ngouoleonel4@gmail.com>
2023-08-23 03:53:10 +0100

    Test
    teste unitaire de hellowold

--- Helloworld1/Helloworld1/src/test/java/com/example/Helloworld1/Helloworld1ApplicationTests.java
+++ Helloworld1/Helloworld1/src/test/java/com/example/Helloworld1/Helloworld1ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.Helloworld1;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.example.service.BusinessService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Helloworld1ApplicationTests {
-
+	@Autowired
+	private BusinessService bs;
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testGetHelloWorld() {
+
+		String expected = "Hello world";
+
+		String result = bs.getHelloWorld().getValue();
+
+		assertEquals(expected, result);
+	}
 }
+
